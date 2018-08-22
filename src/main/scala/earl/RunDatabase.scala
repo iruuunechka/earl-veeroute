568ae302cabd87fe5c03edbebe960d0c0bece2d3
Irene Petrova <iruuunechka@gmail.com>
2018-08-22 15:24:39 +0300

    Spot and improve reading databases a tiny bit.

--- src/main/scala/earl/RunDatabase.scala
+++ src/main/scala/earl/RunDatabase.scala
@@ -2,11 +2,13 @@ package earl
 
 import java.io.File
 import java.nio.file._
+import java.util.stream.Collectors
 
 import scala.collection.JavaConverters._
+
 import io.circe.generic.auto._
-import io.circe.syntax._
 import io.circe.parser._
+import io.circe.syntax._
 
 case class RunDatabase(
   problemName: String,
@@ -27,7 +29,7 @@ object RunDatabase {
   case class OptimizationAct(source: Int, target: Int, optimizer: Int, firstObjective: Int, time: Long)
 
   def load(filename: String): RunDatabase = {
-    val fileContents = Files.readAllLines(Paths.get(filename)).asScala.mkString("\n")
+    val fileContents = Files.lines(Paths.get(filename)).collect(Collectors.joining("\n"))
     decode[RunDatabase](fileContents) match {
       case Left(th) => throw th
       case Right(db) => db
