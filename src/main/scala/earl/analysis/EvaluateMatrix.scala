4218095acee44a831af24b6742a1a865d9c3320c
Irene Petrova <iruuunechka@gmail.com>
2018-08-29 06:43:54 +0300

    Rename a variable so that it does not treated as a typo.

--- src/main/scala/earl/analysis/EvaluateMatrix.scala
+++ src/main/scala/earl/analysis/EvaluateMatrix.scala
@@ -14,8 +14,8 @@ object EvaluateMatrix {
       val allCells = for (o <- 0 until q.firstSize; f <- 0 until q.secondSize) yield (o, f)
       val argMax = allCells.maxBy(p => q(p._1, p._2))
       val argMaxV = q(argMax._1, argMax._2)
-      val nArgMaxs = allCells.count(p => q(p._1, p._2) >= argMaxV - 1e-9)
-      println(s"[info] arg max: count = $nArgMaxs value = $argMaxV")
+      val argMaxCount = allCells.count(p => q(p._1, p._2) >= argMaxV - 1e-9)
+      println(s"[info] arg max: count = $argMaxCount value = $argMaxV")
       println(s"[info]    optimizer: ${example.optimizers(argMax._1)}")
       println(s"[info]    function:  ${example.objectives(argMax._2)}")
     }
