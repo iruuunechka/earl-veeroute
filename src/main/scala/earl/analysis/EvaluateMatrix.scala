623dcd5214ce9687926fb0f55db1d897f789c991
Irene Petrova <iruuunechka@gmail.com>
2018-08-29 07:01:27 +0300

    Implement computing rank sums over decisions.

--- src/main/scala/earl/analysis/EvaluateMatrix.scala
+++ src/main/scala/earl/analysis/EvaluateMatrix.scala
@@ -1,5 +1,7 @@
 package earl.analysis
 
+import scala.collection.mutable.{HashMap => MuHashMap}
+
 import earl.RunDatabase
 import earl.reinforce.QMatrix
 
@@ -9,15 +11,15 @@ object EvaluateMatrix {
     println(s"[info] ${dbs.size} databases, ${dbs.iterator.map(_.acts.size).sum} acts in total")
     if (dbs.length > 0) {
       val example = dbs(0)
-      val q = QMatrix.fromDatabases(dbs, example.objectives, example.optimizers)
-      println(s"[info] Q-matrix has ${q.firstSize} rows/optimizers and ${q.secondSize} columns/functions")
-      val allCells = for (o <- 0 until q.firstSize; f <- 0 until q.secondSize) yield (o, f)
-      val argMax = allCells.maxBy(p => q(p._1, p._2))
-      val argMaxV = q(argMax._1, argMax._2)
-      val argMaxCount = allCells.count(p => q(p._1, p._2) >= argMaxV - 1e-9)
-      println(s"[info] arg max: count = $argMaxCount value = $argMaxV")
-      println(s"[info]    optimizer: ${example.optimizers(argMax._1)}")
-      println(s"[info]    function:  ${example.objectives(argMax._2)}")
+      val allCells = for (o <- example.optimizers.indices; f <- example.objectives.indices) yield (o, f)
+      val cellSumRank = new MuHashMap[(Int, Int), Int]
+      for (db <- dbs) {
+        val q = QMatrix.fromDatabases(Seq(db), example.objectives, example.optimizers)
+        val sortedCells = allCells.sortBy(p => -q.apply(p._1, p._2))
+        sortedCells.zipWithIndex.foreach(p => cellSumRank.update(p._1, cellSumRank.getOrElse(p._1, 0) + p._2))
+      }
+      println(s"Average sum-rank: ${dbs.size * allCells.size / 2}")
+      cellSumRank.toIndexedSeq.sortBy(_._2).foreach(p => println(s"${p._2} => ${example.optimizers(p._1._1)}, ${example.objectives(p._1._2)}"))
     }
   }
 }
